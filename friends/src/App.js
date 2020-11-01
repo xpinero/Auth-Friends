a6b693acf762bfdbb1b1577f1d43df6bb43adf7d
Xaver Pinero <xaver.pinero@gmail.com>
2020-11-01 18:29:31 -0500

    some components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import LoginPage from './components/LoginPage';
+import FriendsPage from './components/FriendsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/friends">
+            <FriendsPage />
+          </Route>
+        
+          <Route path="/">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/FriendsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsPage = () => {
+  return (
+    <div>
+    Friends
+    </div>
+  )
+};
+
+export default FriendsPage;
